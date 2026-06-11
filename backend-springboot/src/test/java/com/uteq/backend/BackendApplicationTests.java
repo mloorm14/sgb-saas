3cf422a7c4bb507a7281b87eb2949e54661df016
mloorm14 <mloorm14@uteq.edu.ec>
2026-06-11 01:02:25 -0500

    feat: agregar configuracion base (application.yml, migracion V1, docker-compose, ci) y ajustar test de contexto

--- backend-springboot/src/test/java/com/uteq/backend/BackendApplicationTests.java
+++ backend-springboot/src/test/java/com/uteq/backend/BackendApplicationTests.java
@@ -3,11 +3,19 @@ package com.uteq.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration," +
+        "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration," +
+        "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration," +
+        "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration," +
+        "org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration"
+})
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
