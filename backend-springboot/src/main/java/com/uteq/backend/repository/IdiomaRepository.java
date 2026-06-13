bdfc2cd78d8bd9100cffecbbccc96e0b6ec274d0
Irvin <icajasi@msuteq.edu.ec>
2026-06-13 10:08:37 -0500

    feat: agregar repository JPA Idioma

---
+++ backend-springboot/src/main/java/com/uteq/backend/repository/IdiomaRepository.java
@@ -0,0 +1,8 @@
+package com.uteq.backend.repository;
+
+import com.uteq.backend.entity.Idioma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IdiomaRepository extends JpaRepository<Idioma, Long> {}
