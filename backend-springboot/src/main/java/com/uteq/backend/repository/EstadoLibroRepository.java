5a33ca43cdeb42486eec365b109a9ab6789852d7
Irvin <icajasi@msuteq.edu.ec>
2026-06-20 02:32:59 -0500

    fix(repository): corregir nombres de archivos de repositorios

---
+++ backend-springboot/src/main/java/com/uteq/backend/repository/EstadoLibroRepository.java
@@ -0,0 +1,8 @@
+package com.uteq.backend.repository;
+
+import com.uteq.backend.entity.EstadoLibro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoLibroRepository extends JpaRepository<EstadoLibro, Long> {}
